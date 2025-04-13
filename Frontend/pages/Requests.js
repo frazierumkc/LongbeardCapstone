49f4381f1e5b1003c6ca31403457444aa50e20a9
frazierumkc <144606877+frazierumkc@users.noreply.github.com>
2025-04-13 17:24:34 -0500

    Delete Frontend/pages/Requests.js

--- Frontend/pages/Requests.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Requests = () => {
-  return (
-    <h1 style={{ textAlign: "center", marginTop: "50px" }}>Requests Page</h1>
-  );
-};
-
-export default Requests;
