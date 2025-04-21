8128029b89ccbca95989326bd3e2218da0317b29
frazierumkc <144606877+frazierumkc@users.noreply.github.com>
2025-04-21 12:08:50 -0500

    Add files via upload

---
+++ Backend/src/main/java/com/rest/splitter/dto/ExpenseDTO.java
@@ -0,0 +1,13 @@
+package com.rest.splitter.dto;
+
+public class ExpenseDTO {
+    public String title;
+    public Float cost;
+    public String date;
+    public String time;
+    public String partner;
+    public String method;
+    public String value;
+    public Float partnerShare;
+    public Float youShare;
+}
