fc898d2ee234b38b3b7092d9cba993c54af1c08d
DavidFF6h <113874835+DavidFF6h@users.noreply.github.com>
2025-05-01 18:23:47 -0500

    Add files via upload

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,7 +5,6 @@ import Dashboard from "./pages/Dashboard";
 import Contacts from "./pages/Contacts";
 import History from "./pages/History";
 import Profile from "./pages/Profile";
-import Settings from "./pages/Settings";
 
 const App = () => {
   return (
@@ -16,7 +15,6 @@ const App = () => {
         <Route path="/contacts" element={<Contacts />} />
         <Route path="/history" element={<History />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/settings" element={<Settings />} />
         <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
